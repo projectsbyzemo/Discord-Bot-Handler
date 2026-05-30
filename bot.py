4730fae22e1535b95aeba56c4d817055d18acb07
haze <prj.rblx@gmail.com>
2026-05-29 22:03:43 -0400

    Add ping command (0.1.1)
    Adds a simple ping command that replies with latency and mentions the author. Also remove explicit intents.message_content assignment (now using default intents).

--- bot.py
+++ bot.py
@@ -12,18 +12,24 @@ import certifi
 os.environ.setdefault("SSL_CERT_FILE", certifi.where())
 
 import discord
+from discord import app_commands
+from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
 
 token = os.getenv('token')
 
-# Client(s)
+# Client
 intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='?', intents=intents)
 
 @client.event 
 async def on_ready():
-    print(f"{clientName} ({version}) is connected to Discord.")
+    print(f"{clientName} ({version}) is online.")
+
+## Commands
+@client.command()
+async def ping(ctx: commands.Context):
+    await ctx.send(f"Pong! {client.latency * 1000:.2f} ms ||{ctx.author.mention}||")
 
 client.run(token)
